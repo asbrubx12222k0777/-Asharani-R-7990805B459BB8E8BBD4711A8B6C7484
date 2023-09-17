fb97f1bf52c37baf77703e8708ea569e4a867bd1
asbrubx12222k0777 <144273118+asbrubx12222k0777@users.noreply.github.com>
2023-09-17 12:17:32 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,21 +1,55 @@
-# Python program to check if year is a leap year or not
+class BankAccount:
+    def __init__(self, account_number, date_of_opening, balance, customer_name):
+        self.account_number = account_number
+        self.date_of_opening  = date_of_opening 
+        self.balance = balance
+        self.customer_name = customer_name
+        
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"${amount} has been deposited in your account.")
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= amount
+            print(f"${amount} has been withdrawn from your account.")
+            
+    def check_balance(self):
+        print(f"Current balance is ${self.balance}.")
+        
+    def print_customer_details(self):
+        print("Name:", self.customer_name)
+        print("Account Number:", self.account_number)
+        print("Date of opening:", self.date_of_opening)
+        print(f"Balance: ${self.balance}\n")   
 
-year = 2000
+# Input customer details
+ac_no_1 = BankAccount(2345, "01-01-2011", 1000, "Toninho Takeo")
+ac_no_2 = BankAccount(1234, "11-03-2011", 2000, "Astrid Rugile")
+ac_no_3 = BankAccount(2312, "12-01-2009", 3000, "Orli Kerenza")
+ac_no_4 = BankAccount(1395, "01-01-2011", 3000, "Luciana Chika")
+ac_no_5 = BankAccount(6345, "01-05-2011", 4000, "Toninho Takeo")
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+print("Customer Details:")
+ac_no_1.print_customer_details()
+ac_no_2.print_customer_details()
+ac_no_3.print_customer_details()
+ac_no_4.print_customer_details()
+ac_no_5.print_customer_details()
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+print("=============================")
+ac_no_4.print_customer_details()
+# Current balance is $3000.
+# $1000 has been deposited in your account.
+ac_no_4.deposit(1000)
+ac_no_4.check_balance()
+# Your current balance $4000.
+# You want to withdraw $5000
+ac_no_4.withdraw(5000)
+# Output:
+# Insufficient balance.
+#The customer withdraw $3400.
+ac_no_4.withdraw(3400)
+ac_no_4.check_balance()
